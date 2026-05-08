92e7d58c147528d92367038394be72d362568dbf
Rushan <Krutov315@mail.ru>
2026-05-08 12:59:05 +0300

    fix: typo

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, UTC
 import os
 import socket
 
@@ -9,7 +9,7 @@ app = FastAPI(title="Zero-Downtime Traffic Shifter")
 APP_VERSION = os.getenv("APP_VERSION", "dev")
 APP_COLOR = os.getenv("APP_COLOR", "blue")
 HOSTNAME = socket.gethostname()
-STARTED_AT = datetime.utcnow().isoformat() + "Z"
+STARTED_AT = datetime.now(UTC).strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
 @app.get("/")
